0f39afa0cae5795d192aeffbf3cbb5fdf93c651e
Rachaaoun <53050847+Rachaaoun@users.noreply.github.com>
2021-10-24 21:35:49 +0200

    s

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -79,7 +79,7 @@ class UserController extends AbstractController
        // dd($users);
         return $this->render('user/etudiant_list.html.twig', [
             'controller_name' => 'UserController',
-            'enseignants'=>$userRepository->findByRoles(['["Role_USER"]'])
+            'enseignants'=>$userRepository->findBy(["ROLE_USER"])
          //   'enseignants'=>$userRepository->findAll()
         ]);
     }
